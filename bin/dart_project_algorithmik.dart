1d520fa88d9f4544ab0f6dd2a55da68830a8d8e4
felix_kuehn <felix_kuehn@mein.gmx>
2026-09-15 14:00:46 +0200

    small changes

--- bin/dart_project_algorithmik.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_project_algorithmik/dart_project_algorithmik.dart' as dart_project_algorithmik;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_project_algorithmik.calculate()}!');
-}
