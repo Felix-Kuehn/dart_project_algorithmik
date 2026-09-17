f982804e0744ca97f0ac7e81d604707cb642d57a
felix_kuehn <felix_kuehn@mein.gmx>
2026-09-17 09:33:07 +0200

    Added decompression function

--- lib/compress_functions.dart
+++ lib/compress_functions.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
+import 'dart:math';
 
 bool validityCheckCompress(String chain){
   return RegExp(r'[\d\W]').hasMatch(chain); //if true is returned, Validity check is failed //
 }
 
 bool validityCheckDecompress(String chain){
-  return !RegExp(r'[a-zA-Z]').hasMatch(chain) || RegExp(r'\W').hasMatch(chain);
+  return !RegExp(r'[a-zA-Z]').hasMatch(chain) || RegExp(r'\W').hasMatch(chain) || !RegExp(r'\d').hasMatch(chain);
 }
 
   String userchainComp ([String a = '0']){
@@ -49,7 +50,7 @@ bool validityCheckDecompress(String chain){
       else {
         validityCheckDecompress(a);
           if (validityCheckDecompress(a) == true){
-            print('Ungültige Eingabe. Muss Buchstaben enthalten und keine Sonderzeichen.');
+            print('Ungültige Eingabe. Muss Buchstaben und Zahlen enthalten und keine Sonderzeichen.');
 
           }
             return a;
@@ -69,7 +70,7 @@ bool validityCheckDecompress(String chain){
 
     /* print(match.group(0)!.length);
     print(match.group(0));    */       // 0 points to the square bracket number in RegExp(). 0 means everything, 1 to the first bracket, and f.e. to the second bracket. As i have only one it says out of range. //
-      sorted += ('${match.group(0)!.length}${match.group(1)}'); // += puts everything next to eachother instead of under each other //
+      sorted += ('${match.group(1)}${match.group(0)!.length}'); // += puts everything next to eachother instead of under each other //
   
     lastEnd = match.end;
   
@@ -79,3 +80,41 @@ bool validityCheckDecompress(String chain){
 
   return sorted;
    }
+
+    String decompress (String input){
+
+    input = input.toUpperCase();
+    
+    final list = RegExp(r'([A-Z])(\d+)');
+
+return input.replaceAllMapped(list, (match) {
+    final letter = match.group(1)!;
+    final count = int.parse(match.group(2)!);
+    return letter * count;
+});
+
+    }
+    
+
+ String randomTestString(){
+    final random = Random();
+
+    const allChars = 'AaBbCcDdMmNnOoPpQqRrSsTtUuVvWwXxYyZzEeFfGgHhIiJjKkL';
+
+
+    String result = '';
+
+    for (int i = 0; i < 50; i++) {
+      if (i > 0 && random.nextDouble() < 0.3) {
+        // 30 % Wahrscheinlichkeit:
+        // vorheriges Zeichen wiederholen
+        result += result[i - 1];
+      } else {
+        // normales zufälliges Zeichen
+        result += allChars[random.nextInt(allChars.length)];
+      }
+    }
+
+    print(result);
+    return result;
+  }
